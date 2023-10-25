a945592062f18412d4b7a01b063a61c5352ab39a
shettyshreyas-9 <shreyasshetty925@gmail.com>
2023-10-25 19:08:44 +0530

    Adding the app file

---
+++ app.py
@@ -0,0 +1,12 @@
+# library for guis
+from tkinter import *
+
+
+class NLPApp:
+    def __init__(self):
+        # load gui of login
+        self.root = Tk()
+        self.root.mainloop()
+
+
+nlp= NLPApp()
