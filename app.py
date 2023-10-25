c4a7a9d61cc7764e83a7a7d3de46665f6fd5c191
shettyshreyas-9 <shreyasshetty925@gmail.com>
2023-10-25 19:20:59 +0530

    Added title & ico img

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ class NLPApp:
     def __init__(self):
         # load gui of login
         self.root = Tk()
+        self.root.title('NLPApp')
+        self.root.iconbitmap('resources/luffy_fev.ico')
         self.root.mainloop()
 
 
