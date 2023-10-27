8c2bbe2d3739c7cc714f4b0f1a95c6c5fbeb98e1
shettyshreyas-9 <shreyasshetty925@gmail.com>
2023-10-27 12:46:45 +0530

    Created method login_gui & Added heading of the App

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ class NLPApp:
         self.root = Tk()
         self.root.title('NLPApp')
         self.root.iconbitmap('resources/luffy_fev.ico')
+        self.root.geometry('350x600')
+        self.root.configure(bg='#34495E')
+
+        self.login_gui()
+
         self.root.mainloop()
+
+    def login_gui(self):
+        heading= Label(self.root,text='NLPApp',bg='#34495E',fg='white')
+        heading.pack(pady=(30,30))
+        heading.configure(font=('verdana',24,'bold'))
 
 
 nlp= NLPApp()
