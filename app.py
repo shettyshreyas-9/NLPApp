af79850ebe77214f8fe1391a84be0ad82cb0cf7f
shettyshreyas-9 <shreyasshetty925@gmail.com>
2023-10-27 14:00:55 +0530

    Added Redirect page for Register & back to Login page

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ class NLPApp:
         self.root.mainloop()
 
     def login_gui(self):
+
+        self.clear()
+
         # For Heading
         heading= Label(self.root,text='NLPApp',bg='#34495E',fg='white')
         heading.pack(pady=(30,30))
@@ -45,8 +48,59 @@ class NLPApp:
 
         # label3 = Label(self.root,text='Not a member?')
         # label3.pack(pady=(20,10))
-        redirect_btn = Button(self.root,text='Register')
-        redirect_btn.pack(pady=(20,10))
+        redirect_now_btn = Button(self.root,text='Register Now', command= self.register_gui)
+        redirect_now_btn.pack(pady=(20,10))
+
+    # command used to redirect
+    def register_gui(self):
+        self.clear()
+
+        # For Heading
+        heading= Label(self.root,text='NLPApp',bg='#34495E',fg='white')
+        heading.pack(pady=(30,30))
+        heading.configure(font=('verdana',24,'bold'))
+
+        # For Name
+
+        label0 = Label(self.root,text='Enter Name')
+        label0.pack(pady=(10,10))
+
+        self.name_input = Entry(self.root,width=40)
+        self.name_input.pack(pady=(5,10),ipady=4)
+
+        # For Email
+
+        label1 = Label(self.root,text='Enter Email')
+        label1.pack(pady=(10,10))
+
+        self.email_input = Entry(self.root,width=40)
+        self.email_input.pack(pady=(5,10),ipady=4)
+
+        # For password
+
+        label2 = Label(self.root,text='Enter Password')
+        label2.pack(pady=(10,10))
+
+        self.password_input = Entry(self.root,width=40,show='*')
+        self.password_input.pack(pady=(5,10),ipady=4)
+
+        # Button for Register
+        register_btn = Button(self.root,text='Register',width=15,height=2)
+        register_btn.pack(pady=(10,10))
+
+        # For Registering
+
+        # label3 = Label(self.root,text='Not a member?')
+        # label3.pack(pady=(20,10))
+        login_redirect_btn = Button(self.root, text='Login Here',command=self.login_gui)
+        login_redirect_btn.pack(pady=(20,10))
+
+
+    # clearing screen function
+    def clear(self):
+        # clear the existing gui
+        for i in self.root.pack_slaves():
+            i.destroy()
 
 
 
